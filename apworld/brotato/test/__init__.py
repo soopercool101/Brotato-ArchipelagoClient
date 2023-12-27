c89a18968c6c9da620fdb1bcf05d43bc76d64546
Spenser Haddad <spenser.haddad@gmail.com>
2023-12-27 18:30:28 -0500

    Remove unnecessary world_setup

--- apworld/brotato/test/__init__.py
+++ apworld/brotato/test/__init__.py
@@ -1,15 +1,10 @@
 from typing import ClassVar
 from .. import BrotatoWorld
 
-from test.TestBase import WorldTestBase
+from test.bases import WorldTestBase
 
 
 class BrotatoTestBase(WorldTestBase):
     game = "Brotato"
     world: BrotatoWorld
     player: ClassVar[int] = 1
-
-    def world_setup(self, seed: int | None = None) -> None:
-        super().world_setup(seed=seed)
-        if self.constructed:
-            self.world = self.multiworld.worlds[self.player]
